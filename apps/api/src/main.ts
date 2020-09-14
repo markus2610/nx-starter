0c378f444f3d094fc64d2ceb3ad58d4365de4018
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-09-14 20:50:19 +0200

    Add security middlewares

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,11 +5,23 @@
 
 import { Logger } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-
+import * as csurf from 'csurf'
+import * as rateLimit from 'express-rate-limit'
+import * as helmet from 'helmet'
 import { AppModule } from './app/app.module'
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
+    app.use(helmet())
+    app.enableCors()
+    app.use(csurf())
+    app.use(
+        rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 100, // limit each IP to 100 requests per windowMs
+        }),
+    )
+
     const globalPrefix = 'api'
     app.setGlobalPrefix(globalPrefix)
     const port = process.env.PORT || 3333
