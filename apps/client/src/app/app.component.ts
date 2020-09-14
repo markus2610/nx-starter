f8b78a3eb0697ad239330f3da9160b855e56e6f9
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-09-14 08:42:00 +0000

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@nx-starter/api-interfaces';
+
+@Component({
+  selector: 'nx-starter-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}
