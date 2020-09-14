a2a4b2a550500ff96ad08549c6137ff2549238bc
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-09-14 09:39:43 +0000

    Add lint-stage and husky for precommit

--- libs/api-interfaces/jest.config.js
+++ libs/api-interfaces/jest.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  name: 'api-interfaces',
-  preset: '../../jest.config.js',
-  globals: {
-    'ts-jest': {
-      tsConfig: '<rootDir>/tsconfig.spec.json',
+    name: 'api-interfaces',
+    preset: '../../jest.config.js',
+    globals: {
+        'ts-jest': {
+            tsConfig: '<rootDir>/tsconfig.spec.json',
+        },
     },
-  },
-  transform: {
-    '^.+\\.[tj]sx?$': 'ts-jest',
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/libs/api-interfaces',
-};
+    transform: {
+        '^.+\\.[tj]sx?$': 'ts-jest',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+    coverageDirectory: '../../coverage/libs/api-interfaces',
+}
