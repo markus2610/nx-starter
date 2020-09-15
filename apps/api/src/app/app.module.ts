d010cbb8972d536398d6b21860f7bdd79dd0308d
Touhidur Rahman <touhidur.rahman@check24.de>
2020-09-15 09:55:05 +0200

    Rename env file

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+import { environment } from '../environments/environment'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { UsersModule } from './users/users.module'
 
 @Module({
-    imports: [ConfigModule.forRoot(), UsersModule],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: environment.production ? '.env' : '.dev.env',
+            ignoreEnvFile: false, // make it true in production so that env vars are taken from the OS shell exports
+        }),
+        UsersModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
