4c5a239889b9af4abc9435896bf46fd84d9c1933
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-09-23 20:40:47 +0200

    (client) remove client-e2e

--- apps/webapp/jest.config.js
+++ apps/webapp/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'client',
+    name: 'webapp',
     preset: '../../jest.config.js',
     setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
     globals: {
@@ -12,7 +12,7 @@ module.exports = {
             ],
         },
     },
-    coverageDirectory: '../../coverage/apps/client',
+    coverageDirectory: '../../coverage/apps/webapp',
     snapshotSerializers: [
         'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
         'jest-preset-angular/build/AngularSnapshotSerializer.js',
--- apps/webapp/src/app/app.component.spec.ts
+++ apps/webapp/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import { TestBed, waitForAsync } from '@angular/core/testing'
 import { HttpClientModule } from '@angular/common/http'
+import { TestBed, waitForAsync } from '@angular/core/testing'
 import { AppComponent } from './app.component'
 
 describe('AppComponent', () => {
