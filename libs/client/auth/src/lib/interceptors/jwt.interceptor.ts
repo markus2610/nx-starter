2227f4f88a91eb6ba704a02ba6a4a662774931f3
Touhidur Rahman <touhidrahman1987@gmail.com>
2020-09-23 07:16:41 +0000

    (client) improve jwtinterceptor

--- libs/client/auth/src/lib/interceptors/jwt.interceptor.ts
+++ libs/client/auth/src/lib/interceptors/jwt.interceptor.ts
@@ -8,7 +8,7 @@ import {
 import { Injectable } from '@angular/core'
 import { AuthService } from '@nx-starter/client/data-access'
 import { BehaviorSubject, Observable, throwError } from 'rxjs'
-import { catchError, filter, switchMap, take } from 'rxjs/operators'
+import { catchError, filter, finalize, switchMap, take } from 'rxjs/operators'
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
@@ -18,55 +18,63 @@ export class JwtInterceptor implements HttpInterceptor {
     constructor(private authService: AuthService) {}
 
     intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-        // add auth header with jwt if user is logged in and request is to the api url
-        const currentAccessToken = this.authService.currentTokenValue
-        if (currentAccessToken) {
-            request = this.addToken(request, currentAccessToken)
+        if (!request.headers.has('Content-Type')) {
+            request = request.clone({
+                headers: request.headers.set('Content-Type', 'application/json'),
+            })
         }
-        return next.handle(request)
 
-        //     .pipe(
-        //     catchError((error) => {
-        //         if (error instanceof HttpErrorResponse && error.status === 401) {
-        //             return this.handleUnauthorizedError(request, next)
-        //         } else {
-        //             return throwError(error)
-        //         }
-        //     }),
-        // )
+        // add auth header with jwt if user is logged in and request is to the api url
+        request = this.addToken(request)
+
+        return next.handle(request).pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (error && error.status === 401) {
+                    return this.handleUnauthorizedError(request, next)
+                } else {
+                    return throwError(error)
+                }
+            }),
+        )
     }
 
-    private addToken(request: HttpRequest<unknown>, token: string) {
+    private addToken(request: HttpRequest<unknown>) {
+        // If we do not have a token yet then we should not set the header.
+        // Here we could first retrieve the token from where we store it.
+        if (!this.authService.currentTokenValue) {
+            return request
+        }
+        // If you are calling an outside domain then do not add the token.
+        if (!request.url.match(/www.mydomain.com\//)) {
+            return request
+        }
         return request.clone({
-            setHeaders: {
-                Authorization: `Bearer ${token}`,
-            },
+            headers: request.headers.set(
+                'Authorization',
+                `Bearer ${this.authService.currentTokenValue}`,
+            ),
         })
     }
 
     // when a request is unauthorized, check if the jwt has expired. If so,
     // attempt to get a new token and send the failed request again
     private handleUnauthorizedError(request: HttpRequest<unknown>, next: HttpHandler) {
-        console.log('TCL: this.isRefreshing :', this.isRefreshing) // ! remove
-        if (!this.isRefreshing) {
-            this.isRefreshing = true
-            this.refreshTokenSubject.next(null)
-            console.log('TCL: A :') // ! remove
-
-            return this.authService.refreshToken$().pipe(
-                take(1),
-                switchMap(({ accessToken }) => {
-                    this.isRefreshing = false
-                    this.refreshTokenSubject.next(accessToken)
-                    return next.handle(this.addToken(request, accessToken))
-                }),
-            )
-        } else {
-            console.log('TCL: B :') // ! remove
+        if (this.isRefreshing) {
             return this.refreshTokenSubject.pipe(
                 filter((token) => token !== null),
                 take(1),
-                switchMap((jwt) => next.handle(this.addToken(request, jwt))),
+                switchMap((jwt) => next.handle(this.addToken(request))),
+            )
+        } else {
+            this.isRefreshing = true
+            this.refreshTokenSubject.next(null)
+
+            return this.authService.refreshToken$().pipe(
+                switchMap(({ accessToken }) => {
+                    this.refreshTokenSubject.next(accessToken)
+                    return next.handle(this.addToken(request))
+                }),
+                finalize(() => (this.isRefreshing = false)),
             )
         }
     }
